38d534524b98168032a7d06ee6b0ed66030ee207
[supraditya] <[aditya300100@gmail.com]>
2020-08-17 00:20:44 +0530

    Name prompt, custom message based on prompt added to index.html

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function showTime() {
+let person;
+function begin(num) {
+  if (num === 1) {
+    person = prompt("Please enter your name");
+  }
   function dateTime() {
     var d = new Date();
     var day = d.getDay();
+
     var dayArray = [
       "Sunday",
       "Monday",
@@ -23,6 +28,21 @@ function showTime() {
     if (hr < 10) {
       hr = "0" + hr;
     }
+    if (num === 1) {
+      if (hr >= 0 && hr < 12) {
+        document.getElementById("welcome").textContent =
+          "Good Morning, " + person + ".";
+      }
+      if (hr >= 12 && hr < 16) {
+        document.getElementById("welcome").textContent =
+          "Good Afternoon, " + person + ".";
+      }
+      if (hr >= 16 && hr <= 23) {
+        document.getElementById("welcome").textContent =
+          "Good Evening, " + person + ".";
+      }
+    }
+
     $("#dateline").html(dayArray[day] + ",&nbsp");
     $("#dateline2").html(hr + ":" + min + ":" + sec);
   }
